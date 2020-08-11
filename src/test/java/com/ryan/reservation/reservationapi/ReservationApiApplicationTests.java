67c65e802cc12f1e3d63dbed2fcd5daffdb24747
Ryan Koshy <rkoshy141@gmail.com>
2020-08-11 08:11:10 -0400

    First commit

---
+++ src/test/java/com/ryan/reservation/reservationapi/ReservationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryan.reservation.reservationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
